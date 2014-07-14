51f0f68801bab808306aaec08d19fb1bf68c6e64
unknown <parul@navadasha.com>
2014-07-14 14:28:43 +0200

    fix search on window resize

--- js/extendedsearch.js
+++ js/extendedsearch.js
@@ -1,32 +1,39 @@
  
  $(document).ready(function(){
-$('.typeahead').focus(function()
-		{
-			animSpeed = 800;
-	       /*to make this flexible, storing the current width in an attribute*/
-			navWidth = $('.navbar-nav').width();
-			searchParentWidth = $('.navbar-nav > li.search').css('width');
-			buttonWidth = $('.navbar-nav > li.search button').css('width');
-			
-			//hide the menu
-			$('.navbar-nav').css('background-color','#BF1D19');
-	      	$('.navbar-nav > li.group').css('display','none');
-	    	
-			//set the button width in pixels (instead of percentage as it is by default in CSS)
-	    	$('.navbar-nav > li.search button').css('width', buttonWidth);
-	      
-			//animate the typeahead's parent LI to the width of the navigation bar
-			$('.navbar-nav > li.search').animate({width:navWidth},animSpeed);
-	      
-		}).blur(function()
-		{
-			//reset to the original widths
-			$('.navbar-nav > li.search').animate({width:searchParentWidth},animSpeed);
-			$('.navbar-nav > li.search button').css('width',buttonWidth);
-			$('.navbar-nav > li.search input').css('width','100%');
-			   
-		    //show the menu after the animation completes
-			setTimeout(function(){	$('.navbar-nav > li.group').css('display','block');}, animSpeed);
-		});
+     $('.typeahead').focus(function()
+    			{
+    				//animation speed
+    				animSpeed = 800;
+
+    		        //store the current width
+    				searchParentWidth = ($('.navbar-nav > li.search').css('width'));		
+    				
+    				//hide the menu
+    		      	$('.navbar-nav > li.group').toggle();
+
+    				//animate the typeahead's parent LI to the full available width
+    				$('.navbar-nav > li.search').animate({width:'100%'},animSpeed);
+    				
+    			}).blur(function()
+    			{
+    				searchParentWidth = parseInt(searchParentWidth) - 0.1;
+
+    				//reset to the original widths
+    				$('.navbar-nav > li.search').animate({width:searchParentWidth},animSpeed);
+    			
+    				
+    			    //show the menu after the animation completes
+    				setTimeout(function(){	
+    					$('.navbar-nav > li.group').toggle();
+    					}, animSpeed);
+    			    	buttonWidthPercentage
+
+    					//$('.navbar-nav > li.group').css('display','block');
+    		      	//$('.navbar-nav > li.search').css('width',width);
+    			    /* lookup the original width */
+    		 		//var w = $(this).parent().attr('data-default');
+    			    //$(this).parent().  alert("lkjlø");
+    				//$('.navbar-nav li').css("background-color","red");
+    			});
 
 });
