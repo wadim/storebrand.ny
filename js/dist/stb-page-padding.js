5ea9ba43a1b4458f054863911e6c95d3a108d8ac
Bjørn Christian Tørrissen <bzt@storebrand.no>
2014-07-28 16:50:19 +0200

    Modifying article list template in preparation for re-write of code (Parul).

--- js/dist/stb-page-padding.js
+++ js/dist/stb-page-padding.js
@@ -78,3 +78,71 @@ $("document").ready(function(){
     $(document).bind('keydown', 'shift+l', function(){$('li.login > a').click();});
   }
 })
+
+// Handling automatically generated toc lists for article pages
+$(document).ready(function() {
+  var i = 0;
+  var j = 0;
+  // find all the major articles 
+  var tocHeaders= $('.toc-header');
+  for(i=0; i<tocHeaders.length; i++) {
+    //retrieve heading(h3) content and id for each heading to be included in toc
+    var h3text = $(tocHeaders[i]).find('h3').html();
+    var h3id = $(tocHeaders[i]).find('h3').attr("id");
+    
+    // append article heading text as links in the toc-header list
+    $('.toc-list .items').append('<li><a href="#'+h3id+'">'+h3text+'</a></li>');
+    
+    // find all sub-headers inside each toc-header
+    var allh4= $(tocHeaders[i]).find('h4');
+    
+    // make a list of all sub-headers for each toc-header
+    var h4list = '<ul style="margin-left: 20px">';
+    for( j=0; j<allh4.length;j++) {
+      var h4text= $(allh4[j]).html();
+      var h4id= $(allh4[j]).attr("id");
+      h4list=h4list+'<li><a href="#'+h4id+'">'+h4text+'</a></li>';
+    }
+    
+    h4list = h4list+'</ul>';
+    //append these sub-headers to the toc-header list
+    $('.toc-list .items').append(h4list);
+  }
+	 	
+  // Performs a smooth page scroll to an anchor on the same page.
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          scrollToPosition = target.offset().top;					
+					// Checking if the navigation header is fixed. If yes, set the scrollto position by subtracting the height of the fixed header
+					if($('.navbar-fixed-top').css("display") == "block") { 
+            scrollToPosition -= $('.navbar-fixed-top').height(); 
+          }
+          $('html,body').animate({ scrollTop: scrollToPosition }, 1000);
+          // Highlighting the clicked heading for a brief period to make it easier to see what just happened.
+          $(target).animate({backgroundColor:"yellow"},1000);
+          $(target).animate({backgroundColor:"white"},1000);
+          return false;
+	 		  }
+      }
+    });
+  });
+  
+});
+
+$(document).ready(function(){ 
+  // hide and show articles list 
+  $('.toc-list p').click(function() {
+    // if the media screen width is large, then don't do anything
+    if( $( window ).width() <= 992) {
+      // the following will apply for mobile view
+      $('.toc-list .items').slideToggle();
+      $('.toc-list p span').toggleClass(" red chevron-down chevron-up");
+    }
+  });
+});
+
+// End of handling automatically generated toc lists for article pages
