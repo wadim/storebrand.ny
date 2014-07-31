d0855e7ab5baac0763a209da029cf68f508c0f22
parulgit <parul@navadasha.com>
2014-07-31 14:24:19 +0200

    Common search method to be used everywhere
    This is be later added to a common search js

--- js/search.js
+++ js/search.js
@@ -1,56 +1,21 @@
-$(document).ready( function(){
-/**
- * Initialize typeahead.js
- */
-$('.typeahead').typeahead({
-	name: $(this).attr('id'),
-	minLength:3,
-	prefetch: {
-		// http://www.geonames.org/export/geonames-search.html
-		url: 'data/sample.json',
-		filter: function(parsedResponse) {
-			var result = [];
-			for (var i=0; i<parsedResponse.keywords.length; i++) {
-				var Id = parsedResponse.keywords[i].Id;
-				result.push({
-					name: parsedResponse.keywords[i].keyword,
-					value: parsedResponse.keywords[i].keyword,
-					categoryName: parsedResponse.keywords[i].categoryName,
-					
-				});
-			}
-			return result;
-		}
-	},
-	template: [
-	    '<p class="geo-country text-muted">{{categoryName}}</p>',
-		'<p class="geo-name">{{name}}</p>'
-		
-	].join(''),
-	engine: Hogan
-});
-
-$('.typeahead').focus(function()
-
-		           		{
-
-		            	      /*to make this flexible, storing the current width in an attribute*/
-
-		           			$(this).parent().attr('data-default', $(this).parent().css('width'));
-
-		           		    $(this).parent().animate({ width: 300 }, 'slow');
-
-		           		}).blur(function()
-		            		{
-
-		            		    /* lookup the original width */
-
-	            		    var w = $(this).parent().attr('data-default');
-		            		    $(this).parent().animate({ width: w }, 'slow');
-
-		            		});
-
-		          
 
 
-});
+// This Javascript file containes functions required for search
+// Later this can be merged with the other search JS files like extendedsearch.js
+
+
+function checkSearch(searchterm) {
+	//alert(searchbox.value);
+    searchGoogleBootstrap(searchterm.value);
+}
+
+function searchGoogleBootstrap(sterm) {
+  //var sterm =document.getElementById("searchterm").value;
+  var reg1 = new RegExp("\"","g"); 
+  var reg2 = new RegExp("'", "g");
+  var reg3 = new RegExp("<", "g");
+  sterm = sterm.replace(reg1, "");
+  sterm = sterm.replace(reg2, "");
+  sterm = sterm.replace(reg3, "");
+  window.location = "o2w-test-search-result.html?q="+sterm;
+}	;
