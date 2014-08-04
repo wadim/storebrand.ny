4d3055a9dbfd072ace89a2456af3d6e864232108
parulgit <parul@navadasha.com>
2014-08-04 16:10:13 +0200

    Event tracking: FAQs

--- js/dist/stb-analytics.js
+++ js/dist/stb-analytics.js
@@ -1,6 +1,15 @@
-// capture the clicking of the article links
+
 $(document).ready(function(){
+	
+  // tracking article links
   $('.items a').click(function(){
 	ga('send', 'event', 'ArticleList', document.title, $(this).text());
   });
+  
+  // tracking FAQ's
+  $('.accordion a').click(function(){
+    var accordionHeader = $(this).clone().children().remove().end().text().substr(0,20);
+    ga('send', 'event', 'FAQs', document.title,accordionHeader);
+  });
+  
 });
