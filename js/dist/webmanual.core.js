4ca41d56cc904fa7e2694e3bb8b0d07ef176eaa7
parulgit <parul@navadasha.com>
2014-10-27 15:55:24 +0100

    clickable chevron icons in TOC list: webmanual

--- js/dist/webmanual.core.js
+++ js/dist/webmanual.core.js
@@ -196,5 +196,17 @@ $(document).ready(function() {
               //create the link and return it
               return '<li><a href="#'+headerId+'">'+headerText+'</a></li>';
             }
-        });
-    });
+        }), $(document).ready( function(){
+        	  // small chevron icon in mobile view
+        	  if( $( window ).width() <= 768){
+		        $('.toc-list p span').toggleClass("stb-sprite-small stb-sprite-medium");
+		      }
+		      // hide and show articles list
+		      $('.toc-list p').click(function() {
+		        // if the media screen width is large, then don't do anything
+		        // the following will apply for mobile view
+		        $('.toc-list .items').slideToggle();
+		        $('.toc-list p span').toggleClass("chevron-down red chevron-up");
+		      });
+            });
+      });
