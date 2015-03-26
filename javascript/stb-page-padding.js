7ee6ad41ef56980180c1b50ed4ff5eae4147435a
Johannessen, Vegard <vegard.johannessen@storebrand.no>
2015-03-26 12:04:42 +0100

    DS-1108 Callback: Added $.support.cors = true

--- javascript/stb-page-padding.js
+++ javascript/stb-page-padding.js
@@ -598,6 +598,7 @@
       var phoneNumber = $(form).find('input.phone').val();
       if(!isNaN(phoneNumber) && phoneNumber.length === 8){
         var callbackUrl = $(form).find(".callback-channel").val() + phoneNumber;
+        $.support.cors = true;
         $.ajax({
           url: callbackUrl,
           success: function(data) {
